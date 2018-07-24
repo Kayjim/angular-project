ac3946c0ef0106a57ba511ddd50833ee4566c45b
Jimmy Dang <jdang@empoweredbenefits.com>
2018-07-24 13:47:25 -0400

    created basic app functionality

---
+++ src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({ providedIn: 'root' })
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('MessageService: fetched messages');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`MessageService: fetched message id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Coding Standards' },
+  { id: 12, name: 'UX Development' },
+  { id: 13, name: 'UX Design Process' },
+  { id: 17, name: 'Systems Engineering' },
+  { id: 18, name: 'DevOps' },
+  { id: 19, name: 'Angular functions' },
+  { id: 20, name: 'Ping Pong' }
+];
